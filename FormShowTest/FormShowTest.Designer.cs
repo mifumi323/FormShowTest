e5d9e54a274e45ed6b81c70a1cfe39b8bc8458c3
mifumi323 <mifumi323@gmail.com>
2017-11-14 09:00:16 +0900

    initial commit

---
+++ FormShowTest/FormShowTest.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FormShowTest
+{
+    partial class FormShowTest
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowDialog = new System.Windows.Forms.Button();
+            this.btnShowUsing = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShowDialog
+            // 
+            this.btnShowDialog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShowDialog.Location = new System.Drawing.Point(12, 12);
+            this.btnShowDialog.Name = "btnShowDialog";
+            this.btnShowDialog.Size = new System.Drawing.Size(260, 23);
+            this.btnShowDialog.TabIndex = 0;
+            this.btnShowDialog.Text = "ShowDialog";
+            this.btnShowDialog.UseVisualStyleBackColor = true;
+            this.btnShowDialog.Click += new System.EventHandler(this.btnShowDialog_Click);
+            // 
+            // btnShowUsing
+            // 
+            this.btnShowUsing.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShowUsing.Location = new System.Drawing.Point(12, 41);
+            this.btnShowUsing.Name = "btnShowUsing";
+            this.btnShowUsing.Size = new System.Drawing.Size(260, 23);
+            this.btnShowUsing.TabIndex = 1;
+            this.btnShowUsing.Text = "Show(usingあり)";
+            this.btnShowUsing.UseVisualStyleBackColor = true;
+            this.btnShowUsing.Click += new System.EventHandler(this.btnShowUsing_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShow.Location = new System.Drawing.Point(12, 70);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(260, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show(usingなし)";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // FormShowTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 105);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnShowUsing);
+            this.Controls.Add(this.btnShowDialog);
+            this.Name = "FormShowTest";
+            this.Text = "FormをShowするテスト";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowDialog;
+        private System.Windows.Forms.Button btnShowUsing;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ FormShowTest/FormShowTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormShowTest
+{
+    public partial class FormShowTest : Form
+    {
+        public FormShowTest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowDialog_Click(object sender, EventArgs e)
+        {
+            // よくある例
+            using (var f = new Form())
+            {
+                f.ShowDialog();
+            }
+        }
+
+        private void btnShowUsing_Click(object sender, EventArgs e)
+        {
+            // よくない例
+            using (var f = new Form())
+            {
+                f.Show();
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            // よい例
+            var f = new Form();
+            f.Show();
+        }
+    }
+}
